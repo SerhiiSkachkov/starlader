b5f79b1698130e9dfbd2775ea4c322a0d99fdb9b
Serhi Skachkov <sergeyskachkov01@gmail.com>
2020-03-24 18:00:26 +0200

    Updates

---
+++ precache-manifest.8e6c23a15f2e5c58c1037413841a9c80.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a4c0e9c592368411dcfde373897d15",
+    "url": "/starladergi/index.html"
+  },
+  {
+    "revision": "4aef351f172b7e286ac3",
+    "url": "/starladergi/static/css/main.a6f7ebf7.chunk.css"
+  },
+  {
+    "revision": "a3a87171643a0d56edba",
+    "url": "/starladergi/static/js/2.788c7e29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starladergi/static/js/2.788c7e29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aef351f172b7e286ac3",
+    "url": "/starladergi/static/js/main.610e48f5.chunk.js"
+  },
+  {
+    "revision": "6d2dfff17cdea824b366",
+    "url": "/starladergi/static/js/runtime-main.25ad1d83.js"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/starladergi/static/media/Roboto.58bf8e0b.woff"
+  },
+  {
+    "revision": "69eee0b43d46be3ad91ef75a76ea3959",
+    "url": "/starladergi/static/media/Roboto.69eee0b4.woff2"
+  },
+  {
+    "revision": "c613ca81d36649b260700f3335d8f579",
+    "url": "/starladergi/static/media/Roboto.c613ca81.ttf"
+  },
+  {
+    "revision": "202f9d5222c18d3d3d035035a8c93725",
+    "url": "/starladergi/static/media/Robotoblack.202f9d52.woff2"
+  },
+  {
+    "revision": "8a7ab9f2be329b76881a7cb798f9cc38",
+    "url": "/starladergi/static/media/Robotoblack.8a7ab9f2.woff"
+  },
+  {
+    "revision": "bf7b513ace1a2ac0dbc6d95d2dcef261",
+    "url": "/starladergi/static/media/Robotoblack.bf7b513a.ttf"
+  },
+  {
+    "revision": "15a913e1228cca9b1247b373bdf27e55",
+    "url": "/starladergi/static/media/Robotobold.15a913e1.woff2"
+  },
+  {
+    "revision": "1fc6a83d1e366dfe7babb690d33ba6fb",
+    "url": "/starladergi/static/media/Robotobold.1fc6a83d.ttf"
+  },
+  {
+    "revision": "7bf74824d7a8b3c08fe5956f93e9ce2a",
+    "url": "/starladergi/static/media/Robotobold.7bf74824.woff"
+  },
+  {
+    "revision": "38e1ed9a9ee49d755d20226b0d66588c",
+    "url": "/starladergi/static/media/Robotolight.38e1ed9a.woff2"
+  },
+  {
+    "revision": "611ac98371c2d77c377041e1a0fd87dc",
+    "url": "/starladergi/static/media/Robotolight.611ac983.woff"
+  },
+  {
+    "revision": "63d6f579b0db1fc75cc977a0539ce45f",
+    "url": "/starladergi/static/media/Robotolight.63d6f579.ttf"
+  },
+  {
+    "revision": "38aa81d461c299fa9f0cb27726401996",
+    "url": "/starladergi/static/media/Robotomedium.38aa81d4.ttf"
+  },
+  {
+    "revision": "4a34361b26e9896ea1b5065708f8daea",
+    "url": "/starladergi/static/media/Robotomedium.4a34361b.woff2"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/starladergi/static/media/Robotomedium.6422eac7.woff"
+  },
+  {
+    "revision": "72de91db358fdf015511e4c3c8bc607c",
+    "url": "/starladergi/static/media/Robotothin.72de91db.woff2"
+  },
+  {
+    "revision": "7faf23e11b3c2da59062fffc117e3a40",
+    "url": "/starladergi/static/media/Robotothin.7faf23e1.ttf"
+  },
+  {
+    "revision": "a11d03f7bc9e68d84afdc5eea52ed121",
+    "url": "/starladergi/static/media/Robotothin.a11d03f7.woff"
+  },
+  {
+    "revision": "2090b26b21ff1830d6e317500ed732f4",
+    "url": "/starladergi/static/media/header_bg_sm.2090b26b.jpg"
+  },
+  {
+    "revision": "4e6c83301c03f4c08f7cb4f04237170b",
+    "url": "/starladergi/static/media/header_bg_star_xl.4e6c8330.svg"
+  },
+  {
+    "revision": "e5e57358ecc5ad204eb6be1544f283c1",
+    "url": "/starladergi/static/media/header_bg_xl.e5e57358.jpg"
+  },
+  {
+    "revision": "b342f70dd79e195dbe7e27ff9c550fc1",
+    "url": "/starladergi/static/media/logo-star_begin.b342f70d.svg"
+  },
+  {
+    "revision": "f337b1cc2f79921c582ac88127dd178d",
+    "url": "/starladergi/static/media/logo-star_finish.f337b1cc.svg"
+  },
+  {
+    "revision": "b342e38ec827eb5606190485d121e4d7",
+    "url": "/starladergi/static/media/logo-star_online.b342e38e.svg"
+  }
+]);
